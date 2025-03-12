653b85c7e4d375b426cb56a52f23e1fadb13bd47
Sajjad Murtaza <sajjadmurtaza.nxb@gmail.com>
2025-03-13 06:06:06 +0900

    refactor rspecs

--- ruby/spec/models/article_spec.rb
+++ ruby/spec/models/article_spec.rb
@@ -21,30 +21,5 @@ describe Article do
       article = Article.new(title: 'Test Article')
       expect(article).not_to be_valid
     end
-
-    it 'is not valid with a duplicate title' do
-      Article.create(title: 'Unique Title', content: 'Test Content')
-      article = Article.new(title: 'Unique Title', content: 'Different Content')
-      expect(article).not_to be_valid
-    end
-  end
-
-  describe 'associations' do
-    it 'has many comments' do
-      article = Article.create(title: 'Article with Comments', content: 'Test Content')
-      comment1 = Comment.create(article_id: article.id, content: 'First comment', author_name: 'John')
-      comment2 = Comment.create(article_id: article.id, content: 'Second comment', author_name: 'Jane')
-
-      expect(article.comments.count).to eq(2)
-      expect(article.comments).to include(comment1)
-      expect(article.comments).to include(comment2)
-    end
-
-    it 'deletes associated comments when deleted' do
-      article = Article.create(title: 'Article to Delete', content: 'Test Content')
-      Comment.create(article_id: article.id, content: 'Comment to be deleted', author_name: 'John')
-
-      expect { article.destroy }.to change { Comment.count }.by(-1)
-    end
   end
 end
